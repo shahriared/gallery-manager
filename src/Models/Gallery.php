272028f8ab8a8e10b0945e536f699899417407ea
Shahriar Edalat Zadeh <shahriared@gmail.com>
2023-01-29 13:33:52 +0400

    feat: update gallery model.

--- src/Models/Gallery.php
+++ src/Models/Gallery.php
@@ -9,4 +9,9 @@ class Gallery extends Model
     protected $table = 'galleries';
 
     protected $fillable = ['name', 'description', 'is_published'];
+
+    public function images()
+    {
+        return $this->hasMany(GalleryImage::class, 'gallery_id');
+    }
 }
