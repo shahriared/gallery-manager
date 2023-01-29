6c5e57e6796cf8d11fe29aefcfc4933c7c4f0686
Shahriar Edalat Zadeh <shahriared@gmail.com>
2023-01-29 13:34:16 +0400

    feat: update galleryImage model.

--- src/Models/GalleryImage.php
+++ src/Models/GalleryImage.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class GalleryImage extends Model
 {
     protected $table = 'gallery_images';
+
+    protected $fillable = ['gallery_id', 'name', 'description', 'alt', 'image'];
 }
