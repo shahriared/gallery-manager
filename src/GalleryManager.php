7cb8d32f70149c79d807cd0d6da67f45e6a27e92
Shahriar Edalat Zadeh <shahriared@gmail.com>
2023-01-29 10:09:33 +0400

    feat: add get all, create and update methods for gallery table.

--- src/GalleryManager.php
+++ src/GalleryManager.php
@@ -2,7 +2,22 @@
 
 namespace Shahriared\GalleryManager;
 
+use Shahriared\GalleryManager\Http\Controllers\GalleryController;
+
 class GalleryManager
 {
+    public function getGalleries()
+    {
+        return GalleryController::getAll();
+    }
 
+    public function createGallery($data)
+    {
+        return GalleryController::create($data);
+    }
+
+    public function updateGallery($data)
+    {
+        return GalleryController::update($data);
+    }
 }
--- src/Http/Controllers/GalleryController.php
+++ src/Http/Controllers/GalleryController.php
@@ -2,7 +2,36 @@
 
 namespace Shahriared\GalleryManager\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+use Shahriared\GalleryManager\Models\Gallery;
+
 class GalleryController
 {
+    public static function getAll()
+    {
+        return Gallery::all();
+    }
 
+    public static function create($data)
+    {
+        $validatedData = Validator::make($data, [
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'is_published' => 'required|boolean',
+        ])->validate();
+        
+        return Gallery::create($validatedData);
+    }
+
+    public static function update($data)
+    {
+        $validatedData = Validator::make($data, [
+            'id' => 'required|exists:galleries,id',
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'is_published' => 'required|boolean',
+        ])->validate();
+        
+        return Gallery::where(['id' => $validatedData['id']])->update($validatedData);
+    }
 }
