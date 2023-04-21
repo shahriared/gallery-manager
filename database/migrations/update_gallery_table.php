6846babf2f5b02810247569cb4aa7463cfd106da
Shahriar Edalat Zadeh <57944847+shahriared@users.noreply.github.com>
2023-04-21 14:27:40 +0400

    Create update_gallery_table.php

---
+++ database/migrations/update_gallery_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->string('year')->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->dropColumn('year');
+        });
+    }
+};
